f766f310e6dbeb525d630868acccad83805af110
EL-S <31227083+EL-S@users.noreply.github.com>
2019-02-18 23:51:32 +1100

    Name Input
    A program that takes a name input and outputs a sentence with the name in the console

---
+++ name_input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "";
+    cout << "What's your name? ";
+    cin >> name;
+
+    cout << "Hey " << name << ", have a nice day.";
+    return 0;
+}
